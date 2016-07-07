86a72c862ad13191c8152eaa50a090c5b17f99b5
felippemr <raposo.felippe@gmail.com>
2016-07-07 15:07:07 -0300

    add mongodb client

---
+++ mongo_top/mongodb_client.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from contextlib import contextmanager
+import logging
+import pymongo
+
+LOG = logging.getLogger(__name__)
+
+MONGO_CONNECTION_DEFAULT_TIMEOUT = 5
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+class ConnectionError(Exception):
+    pass
+
+
+class MongoDB(object):
+    def __init__(self, host, port, database,
+                 user, password, timeout=MONGO_CONNECTION_DEFAULT_TIMEOUT):
+        self._host = host
+        self._port = port
+        self._database = database
+        self._user = user
+        self._password = password
+        self._timeout = timeout
+
+    @property
+    def _connection_string(self):
+        return 'mongodb://{user}:{password}@{host}'.format(
+            user=self._user, password=self._password, host=self._host)
+
+    @property
+    def _database_client(self):
+        client = pymongo.MongoClient(
+            self._connection_string,
+            connectTimeoutMS=self._timeout
+        )
+        return getattr(client, self._database)
+
+    def _authenticate(self):
+        return self._database_client.authenticate(
+            self._user, self._password
+        )
+
+    @contextmanager
+    def pymongo(self):
+        client = None
+        try:
+            yield self._authenticate()
+        except pymongo.errors.OperationFailure, e:
+            if e.code == 18:
+                error_message = 'Invalid credentials to database {}: {}'.format(
+                    self._name, self._connection_string)
+                raise AuthenticationError(error_message)
+        except pymongo.errors.PyMongoError, e:
+            error_message = 'Error connecting to database {}: {}'.format(
+                self._name, self._connection_string)
+            raise ConnectionError(error_message)
+        finally:
+            try:
+                if client:
+                    client.close()
+            except Exception:
+                pass
