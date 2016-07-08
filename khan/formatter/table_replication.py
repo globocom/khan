85477320a5c9c329c851456dc5c438490f2594ea
otherpirate <mauro_murari@hotmail.com>
2016-07-08 12:19:04 -0300

    Fix header order

--- khan/formatter/table_replication.py
+++ khan/formatter/table_replication.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from .table import BaseTable
 
 
@@ -5,16 +6,15 @@ class ReplicationTable(BaseTable):
     __formatter_name__ = 'replication'
 
     def __init__(self, max_lines):
-        header = {
-            'Host': 15,
-            'Port': 5,
-            'State': 9,
-            'Last Update': 19,
-            'Delay': 7,
-            'Health': 5,
-            'Priority': 5,
-            'Votes': 5,
-            'Hidden': 5,
-        }
+        header = OrderedDict()
+        header['Host'] = 15
+        header['Port'] = 5
+        header['State'] = 9
+        header['Last Update'] = 19
+        header['Delay'] = 7
+        header['Health'] = 5
+        header['Priority'] = 5
+        header['Votes'] = 5
+        header['Hidden'] = 5
 
         super(ReplicationTable, self).__init__(header, max_lines)
--- khan/formatter/table_top.py
+++ khan/formatter/table_top.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from .table import BaseTable
 
 
@@ -5,14 +6,13 @@ class TopTable(BaseTable):
     __formatter_name__ = 'queries'
 
     def __init__(self, max_lines):
-        header = {
-            'Id': 8,
-            'Op': 8,
-            'wlf': 3,
-            'Yields': 6,
-            'Duration': 8,
-            'collection': 25,
-            'query': 75,
-        }
+        header = OrderedDict()
+        header['Id'] = 8
+        header['Op'] = 8
+        header['wlf'] = 3
+        header['Yields'] = 6
+        header['Duration'] = 8
+        header['collection'] = 25
+        header['query'] = 75
 
         super(TopTable, self).__init__(header, max_lines)
