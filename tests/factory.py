47ff275fc0844124c0b23871a8a2ecd1b406c56a
felippemr <raposo.felippe@gmail.com>
2016-07-08 12:00:05 -0300

    Add initial tests

---
+++ tests/factory.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import pymongo
+
+
+class FakeMongoClient(object):
+    def __init__(self, connection_string, connectTimeoutMS):
+        self._connection_string = connection_string
+        self._connectTimeoutMS = connectTimeoutMS
+
+    def __getattr__(self, name):
+        return Database(name)
+
+
+class Database(object):
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    def authenticate(self, user, password):
+        if '_fails'in self._name:
+            raise pymongo.errors.OperationFailure(Exception(), code=18)
+        return {'user': user, 'password': password}
+
+    def current_op(self):
+        return {'it': 'works'}
+
+    def command(self, command):
+        return {'command': command}
