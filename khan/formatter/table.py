ae2e629257d495633f8c7c6b6db649c6262797be
otherpirate <mauro_murari@hotmail.com>
2016-07-08 11:09:22 -0300

    Static table size

--- khan/formatter/table.py
+++ khan/formatter/table.py
@@ -4,8 +4,25 @@ import time
 from prettytable import PrettyTable
 
 
-MAX_COLUMN_LEN = 80
 DATE_TIME_FORMAT = '%d-%m-%Y %H:%M:%S'
+FIELD_SIZE = {
+    'Host': 15,
+    'Port': 5,
+    'State': 9,
+    'Last Update': 19,
+    'Delay': 7,
+    'Health': 5,
+    'Priority': 5,
+    'Votes': 5,
+    'Hidden': 5,
+    'Id': 8,
+    'Op': 8,
+    'wlf': 3,
+    'Yields': 6,
+    'Duration': 8,
+    'collection': 25,
+    'query': 80,
+}
 
 
 def show(lines):
@@ -19,28 +36,38 @@ def show(lines):
 
     table = PrettyTable()
     table.field_names = lines[0].keys()
+    table.align = "l"
 
     for line in lines:
-        column = 0
+        table_line = 0
         while True:
             values = []
-            for info in line.values():
-                values.append(format_column(info, column))
+            for key in line.keys():
+                values.append(format_column(key, line[key], table_line))
 
             if not any(values):
                 break
 
             table.add_row(values)
-            column += MAX_COLUMN_LEN
+            table_line += 1
 
     print(table)
 
 
-def format_column(info, column):
+def format_column(key, info, table_line):
     if isinstance(info, dt):
         info = info.strftime(DATE_TIME_FORMAT)
+    info = str(info)
 
-    return str(info)[column:column + MAX_COLUMN_LEN]
+    if key in FIELD_SIZE:
+        diff = FIELD_SIZE[key] - len(info)
+        if diff > 0:
+            info += ' ' * diff
+
+        current_size = FIELD_SIZE[key] * table_line
+        return str(info)[current_size:current_size + FIELD_SIZE[key]]
+
+    return ''
 
 
 def format_header():
